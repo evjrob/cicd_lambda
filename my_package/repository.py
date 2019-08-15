bd43c84ecc4557d460ee150996fa8146be252982
Alexey Smirnov <alexey.smirnov@customergauge.com>
2019-08-15 16:15:36 +0200

    init

---
+++ my_package/repository.py
@@ -0,0 +1,24 @@
+import os
+import boto3
+
+
+def _get_dynamo_table(table_name):
+    dynamo_resource = boto3.resource('dynamodb', region_name=os.environ.get('DEPLOY_REGION'))
+    table = dynamo_resource.dynamo_resource.Table(table_name)
+    return table
+
+
+def save_result(number, result):
+    table_name = os.environ.get('DYNAMO_TABLE_NAME')
+    table = _get_dynamo_table(table_name)
+    item = {'key': number, 'value': result}
+    table.put_item(Item=item)
+
+
+def retrieve_result_for(number):
+    table_name = os.environ.get('DYNAMO_TABLE_NAME')
+    table = _get_dynamo_table(table_name)
+    response = table.get_item(Key=number)
+    item = response.get('Item')
+    if item:
+        return item['value']
